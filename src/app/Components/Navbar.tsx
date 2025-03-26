0e5142313594928581ae0bd70fc7f47d926b027d
muynuddinr <muynuddinr@gmail.com>
2025-03-26 13:11:22 +0530

    logo updated

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -263,7 +263,7 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 lg:h-20">
             {/* Logo - Improved styling without white box */}
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 -ml-2 md:-ml-3 lg:-ml-4">
               <Link 
                 href="/" 
                 className="flex items-center transform transition-all duration-300 
